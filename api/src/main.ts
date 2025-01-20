a3f64f10743f1c9eddfa826b52fe44f40bc295dd
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2025-01-20 19:00:36 +0100

    fix: enable validation pipes globally

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { WsIoAdapter } from "./ws/ws-io-adapter";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useWebSocketAdapter(new WsIoAdapter(app));
   app.enableCors();
 
